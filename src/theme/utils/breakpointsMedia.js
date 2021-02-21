96de8b8fd7e7cba24ce839adf7e3e67d70b8800a
Kiko <kikollovet@gmail.com>
2021-02-20 22:15:51 -0300

    Pequena mudança

--- src/theme/utils/breakpointsMedia.js
+++ src/theme/utils/breakpointsMedia.js
@@ -2,8 +2,8 @@ import { css } from 'styled-components';
 import { breakpoints } from '../index';
 
 export default function breakpointsMedia(cssByBreakpoints) {
-  const breikpointsNames = Object.keys(cssByBreakpoints);
-  return breikpointsNames.map((breakpointName) => css`
+  const breakpointsNames = Object.keys(cssByBreakpoints);
+  return breakpointsNames.map((breakpointName) => css`
     @media only screen and (min-width: ${breakpoints[breakpointName]}px) {
       ${cssByBreakpoints[breakpointName]}
     }
